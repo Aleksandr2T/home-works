a6d401eac97df4501012bd5d2fc722da375a316d
Aleksandr2T <aleksand2t@gmail.com>
2022-05-17 22:48:48 +0300

    add test txt

---
+++ hw2/script.js
@@ -0,0 +1,24 @@
+let userAge = 20;
+
+let userName = 'Alex';
+
+let userSurname = 'Alexandrov';
+console.log(userAge);
+console.log(userName);
+console.log(userSurname);
+
+let newUser;
+let shoppingList;
+let userOnline;
+let employeeSalary;
+let mouseCursorCoordinates;
+   
+let greetings = 'Hello World!';
+console.log(greetings);
+
+
+
+const num1 = 3;
+const num2 = 8;
+const result = num1 + num2;
+console.log(`result`, result);
