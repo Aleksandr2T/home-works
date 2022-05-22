caca0f77f9937e676fbb7f052249c0887be3887b
Aleksandr2T <aleksand2t@gmail.com>
2022-05-22 10:40:55 +0300

    add skript js

---
+++ hw3/skript.js
@@ -0,0 +1,16 @@
+
+
+let a = prompt("введите любое числo ");
+let b = prompt("еще одно ");
+
+console.log(`a`, a);
+console.log(`b`, b);
+
+
+if (a > b){
+ alert("первое число больше второго");
+} else if (a < b){
+ alert("второе число больше первого");
+} else if (a = b){
+  alert(" они равны");
+}
