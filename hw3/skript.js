d0eafa1112b9a5819f95553a83b25839279ab186
Aleksandr2T <aleksand2t@gmail.com>
2022-05-24 11:43:40 +0300

    add skript js

--- hw3/skript.js
+++ hw3/skript.js
@@ -1,16 +1,41 @@
 
 
-let a = prompt("введите любое числo ");
-let b = prompt("еще одно ");
+//let a = prompt("введите любое числo ");
+//let b = prompt("еще одно ");
 
-console.log(`a`, a);
-console.log(`b`, b);
+//console.log(`a`, a);
+//console.log(`b`, b);
 
 
-if (a > b){
- alert("первое число больше второго");
-} else if (a < b){
- alert("второе число больше первого");
-} else if (a = b){
-  alert(" они равны");
+//if (a > b){
+// alert("первое число больше второго");
+//} else if (a < b){
+//alert("второе число больше первого");
+//} else if (a = b){
+//  alert(" они равны");
+//}
+
+const num1 = +prompt("введите первое числo");
+const num2 = +prompt("второе");
+const operation = prompt("Какое действие провести + - / * ?");
+let result;
+
+switch (operation) {
+  case '+':
+    result = num1 + num2;
+    break;
+  case '-':
+    result = num1 - num2;
+    break;
+  case '*':
+    result = num1 * num2;
+    break;
+  case '/':
+    result = num1 / num2;
+    break;
 }
+alert(`${result}`);
+
+
+
+
