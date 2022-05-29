ce8be3ab4bc2c056658a1f726207b90f0463464b
Aleksandr2T <aleksand2t@gmail.com>
2022-05-29 16:50:52 +0300

    add script js

---
+++ hw4/sckipt.js
@@ -0,0 +1,34 @@
+// function calculate(operation, a = 12, b = 8){
+//   console.log(`a`, a);
+//   console.log(`b`, b);
+//   console.log(`operation`, operation);
+ 
+//   if (operation === 'plus'){
+//     return a + b;
+//   }else if(operation === 'minus'){
+//     return a - b;
+//   }
+// }
+// calculate  ('minus');
+
+
+// function getSum(){
+//   const num1 = +prompt('Введите первое число');
+//   const num2 = +prompt('Второе');
+//   const sum = num1 + num2;
+//   alert (`Сумма:` +sum);
+// }
+// getSum()
+// getSum()
+
+function identification(){
+  let userName = prompt('Введите ваше имя');
+  let userAge = +prompt('Введите ваш возраст');
+
+    if(userAge > 30 ){
+      alert ('Здравствуйте'+ ' '+ userName );
+    }else if(userAge < 30 ){
+      alert ('Привет'+ ' '+ userName );
+    }
+}
+identification()
