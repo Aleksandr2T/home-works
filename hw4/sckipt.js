dffe2197c342d9290ef8ee58c21d90023137928f
Aleksandr2T <aleksand2t@gmail.com>
2022-06-21 23:43:09 +0300

    git edd

--- hw4/sckipt.js
+++ hw4/sckipt.js
@@ -35,13 +35,9 @@
 
 
 
-function pow(a, b) {
-   let result = a;
-   for (let i = 1; i < b; i++) {
-     result *= a;
-     console.log(result)
-   }
-   return result;
+function pow(a, b=2) {
+   
+   return a ** b;
  }
- pow (5,2);
+ alert (pow (5));
  
